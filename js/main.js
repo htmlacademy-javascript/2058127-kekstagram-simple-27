48b90a14ae95dec68f69f4c7cbb5f92275f43b7c
AlexanderAries <kosarev20001@mail.ru>
2022-10-27 21:35:23 +0300

    module4

--- js/main.js
+++ js/main.js
@@ -1,19 +1,3 @@
-function checkLength(checkString, maxLength) {
-  return checkString.length < maxLength;
-}
-
-checkLength('Проверяемая строка', 100);
-
-
-function getRandomNumber(min, max) {
-  const lower = min;
-  const upper = max;
-  const random = Math.random();
-  return Math.floor(random * (upper - lower + 1) + lower);
-}
-
-getRandomNumber(20, 140);
-
 function getRandomPositiveInteger(a, b) {
   if (a < 0 || b < 0) {
     return NaN;
@@ -24,34 +8,23 @@ function getRandomPositiveInteger(a, b) {
   return Math.floor(result);
 }
 
-function checkStringLength(string, length) {
-  return string.length <= length;
+let indexID = 0;
+
+const createId = function () {
+  indexID++;
+  return indexID;
+};
+
+function createPhoto() {
+  return {
+    id: createId(),
+    url: 'photos / { indexID }.jpg',
+    description: 'Описание фотографии',
+    likes: getRandomPositiveInteger(15, 200),
+    comments: getRandomPositiveInteger(15, 200),
+  };
 }
 
-/*id, число — идентификатор опубликованной фотографии. Это число от 1 до 25. Идентификаторы не должны повторяться.
+const resultArray = Array.from({ length: 25 }, createPhoto);
 
-url, строка — адрес картинки вида photos/{{i}}.jpg, где {{i}} — это число от 1 до 25. Адреса картинок не должны повторяться.
-
-description, строка — описание фотографии. Описание придумайте самостоятельно.
-
-likes, число — количество лайков, поставленных фотографии. Случайное число от 15 до 200.
-
-comments, число — количество комментариев, оставленных другими пользователями к этой фотографии. Случайное число от 0 до 200. */
-
-console.log() {
-  const id = getRandomPositiveInteger(0, 25);
-  const url = photos / getRandomPositiveInteger(0, 25).jpg;
-  const string = String(getRandomPositiveInteger(0, 25));
-  const likes = getRandomPositiveInteger(15, 200);
-  const comments = getRandomPositiveInteger(0, 200);
-}
-
-/* Вторая карточка
-
-console.log() {
-  const id = getRandomPositiveInteger(0, 25);
-  const url = photos / getRandomPositiveInteger(0, 25).jpg;
-  const string = String(getRandomPositiveInteger(0, 25));
-  const likes = getRandomPositiveInteger(15, 200);
-  const comments = getRandomPositiveInteger(0, 200);
-} */
+console.log(resultArray);
